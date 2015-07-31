658dd34b44fd73f506e2cee4c824c1bbc677ba68
Mariem SOMRANI <somranimariem8@gmail.com>
2015-07-31 13:01:03 +0000

    Create teacher-Bag.java

---
+++ teacher-Bag.java
@@ -0,0 +1,12 @@
+public class teacher_bag extends CordovaActivity 
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.init();
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl(Config.getStartUrl());
+        //super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
